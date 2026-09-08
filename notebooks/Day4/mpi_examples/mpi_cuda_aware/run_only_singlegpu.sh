139781bc5edfc73f265db01518d0588de591f632
Carsten Bauer <crstnbr@gmail.com>
2026-09-08 12:43:18 +0200

    mpi_examples

--- notebooks/Day4/mpi_examples/mpi_cuda_aware/run_only_singlegpu.sh
+++ notebooks/Day4/mpi_examples/mpi_cuda_aware/run_only_singlegpu.sh
@@ -1,8 +1,6 @@
 # uncomment this if you are on cluster gpu node
-ml juliahpc
-ml mpi/openmpi
-export OMPI_MCA_mpi_cuda_support=1
-export OMPI_MCA_mca_component_show_load_errors=0
-export JULIA_CUDA_MEMORY_POOL=none
+# ml juliahpc
+# ml openmpi
+# export JULIA_CUDA_MEMORY_POOL=none
 
 mpiexecjl --project -n 4 julia 1_cudampi_singlegpu.jl
--- notebooks/Day4/mpi_examples/run_all.sh
+++ notebooks/Day4/mpi_examples/run_all.sh
@@ -1,8 +1,6 @@
 # uncomment this if you are on cluster gpu node
 # ml juliahpc
-# ml mpi/openmpi
-# export OMPI_MCA_mpi_cuda_support=0
-# export OMPI_MCA_mca_component_show_load_errors=0
+# ml openmpi
 
 for f in *.jl
 do
